ab3e4205521a8b490159b5b4712824ec5744ff59
wxcgeek <wxchello@qq.com>
2021-03-12 09:34:13 +0800

    Update ExoMediaPlayer.java

--- tvLibrary/src/main/java/com/tv/player/exo/ExoMediaPlayer.java
+++ tvLibrary/src/main/java/com/tv/player/exo/ExoMediaPlayer.java
@@ -59,7 +59,16 @@ public class ExoMediaPlayer extends AbstractPlayer implements VideoListener, Pla
     public void initPlayer() {
         mTrackSelector = new DefaultTrackSelector();
         LoadControl loadControl = new LdDefaultLoadControl();
-        mInternalPlayer = new SimpleExoPlayer.Builder(this).setTrackSelector(mTrackSelector).setLoadControl(loadControl).build();
+        mInternalPlayer = new SimpleExoPlayer.Builder(
+                mAppContext,
+                mRenderersFactory == null ? mRenderersFactory = new DefaultRenderersFactory(mAppContext) : mRenderersFactory,
+                mTrackSelector == null ? mTrackSelector = new DefaultTrackSelector(mAppContext) : mTrackSelector,
+                loadControl = new LdDefaultLoadControl(),
+                DefaultBandwidthMeter.getSingletonInstance(mAppContext),
+                Util.getLooper(),
+                new AnalyticsCollector(Clock.DEFAULT),
+                /* useLazyPreparation= */ true,
+                Clock.DEFAULT).setLoadControl(loadControl).build();).setLoadControl(loadControl).build();
         setOptions();
 
         //播放器日志
